0fc02257f42d7d45bf51b15a465f35494ad66aa5
Marina Puzyreva <puzyrevams@gmail.com>
2017-11-13 22:20:58 +0400

    Created project

---
+++ Translator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Translator
+//
+//  Created by MS on 13.11.2017.
+//  Copyright © 2017 Personal Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Translator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Translator
+//
+//  Created by MS on 13.11.2017.
+//  Copyright © 2017 Personal Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
