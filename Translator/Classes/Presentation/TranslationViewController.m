64b6519b4466a9018b05f96c394a4ee3c32308b4
Marina Puzyreva <puzyrevams@gmail.com>
2017-11-16 23:16:03 +0400

    Changed layout on Tranlation Screen. Added table to Favorite Screen

--- Translator/Classes/Presentation/TranslationViewController.m
+++ Translator/Classes/Presentation/TranslationViewController.m
@@ -8,10 +8,11 @@
 
 #import "TranslationViewController.h"
 
-@interface TranslationViewController ()
+@interface TranslationViewController () <UITextViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextView *originalTextView;
 @property (weak, nonatomic) IBOutlet UITextView *translatedTextView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -21,7 +22,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self addHideKeyboardRecognizer];
 }
+
+#pragma mark - Methods
+
+- (void)addHideKeyboardRecognizer {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:tap];
+}
+
+-(void)dismissKeyboard {
+    [self.originalTextView resignFirstResponder];
+}
+
+- (void)translateOriginalText:(NSString *)originalText {
+    self.translatedTextView.text = [NSString stringWithFormat:@"Переведено: %@", originalText];
+}
+
+
+#pragma mark - Text View Delegate
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    [self translateOriginalText:textView.text];
+}
+
+#pragma mark - Actions
+
+- (IBAction)favoriteButtonTapped:(id)sender {
+}
+
 
 
 @end
